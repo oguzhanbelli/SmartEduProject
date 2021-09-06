9f52e765f61cdbb0791daaa335847cafe3b91e01
Oğuzhan Belli <oguzhanbellii@gmail.com>
2021-09-06 13:04:48 +0300

    Workspace created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+    
+  res.status(200).send('İndex Sayfası');
+
+});
+const port = 3000;
+
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
