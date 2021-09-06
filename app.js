6ee37a3ad1b0e10db46bfa995432a70b3ae610d9
Oğuzhan Belli <oguzhanbellii@gmail.com>
2021-09-06 14:15:38 +0300

    MVC & ROUTES update

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+const pageRoute = require('./routes/pageRoute');
+
 const app = express();
+
+// CONNECT DB
+mongoose.connect()
 
 //TEMPLATE ENGİNE
 const ejs = require('ejs');
 
+
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
-
 app.use(express.static('public'));
+//Routes
 
-app.get('/', (req, res) => {
-  res.status(200).render("index",{
-      page_name:'index',
-    });
-});
-
-app.get('/about',(req,res) => {
-    res.status(200).render("about",{
-    page_name:'about',
-  });
-})
+app.use('/', pageRoute);
 const port = 3000;
 
 app.listen(port, () => {
